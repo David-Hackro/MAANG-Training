a2bac89c5d619dd8cba8851b0669f7367c0f26f1
David Hackro <hackro91@gmail.com>
2024-02-09 16:51:10 -0600

    Time: 1 ms (100.00%) | Memory: 58.5 MB (44.64%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -17,22 +17,18 @@ class Solution {
         ListNode s1 = head, s3 = head;
         int counter = 0;
 
-        // Avanzar s1 hasta el k-ésimo nodo
         while (s1 != null && counter < k - 1) {
             s1 = s1.next;
             counter++;
         }
 
-        // Guardar la posición de s1 para el primer nodo
         ListNode temp = s1;
 
-        // Mover s1 al final de la lista
         while (s1.next != null) {
             s1 = s1.next;
             s3 = s3.next;
         }
 
-        // Swap values between s2 y s3
         int tempVal = temp.val;
         temp.val = s3.val;
         s3.val = tempVal;
