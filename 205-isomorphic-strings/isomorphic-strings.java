821cecb11919c2c6d972024748ce4bf4dd1db3ef
David Hackro <hackro91@gmail.com>
2024-04-27 10:07:05 -0600

    Time: 14 ms (30.15%) | Memory: 42.3 MB (61.96%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,21 +1,28 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        // Create a HashMap to map characters from string 's' to characters from string 't'
         HashMap<Character, Character> map = new HashMap<>();
 
+        // Iterate through the characters of strings 's' and 't'
         for(int i = 0; i < s.length(); i++) {
-            char a = s.charAt(i);
-            char b = t.charAt(i);
+            char a = s.charAt(i); // Current character in string 's'
+            char b = t.charAt(i); // Current character in string 't'
 
+            // If character 'a' is not mapped to any character in 't' and vice versa
             if(!map.containsKey(a) && !map.containsValue(b)) {
-                map.put(a, b);
-            } else if (map.containsKey(a) && map.get(a) == b) {
+                map.put(a, b); // Map 'a' to 'b'
+            } 
+            // If 'a' is mapped to 'b', continue with the next pair of characters
+            else if (map.containsKey(a) && map.get(a) == b) {
                 continue;
-            } else {
-                return false;
+            } 
+            // If 'a' is mapped to another character than 'b' or 'a' is not mapped but 'b' is already mapped to another character
+            else {
+                return false; // The strings are not isomorphic
             }
         }
 
-
+        // If we reach here, it means all mappings are valid, so the strings are isomorphic
         return true;       
     }
 }
