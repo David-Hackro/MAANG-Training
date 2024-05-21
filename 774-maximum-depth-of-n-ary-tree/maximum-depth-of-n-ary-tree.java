b2586965f53e8a5f5e451c9034a3a327f372b495
David Hackro <hackro91@gmail.com>
2024-05-21 07:47:52 -0600

    Time: 0 ms (100.00%) | Memory: 44.2 MB (16.65%) - LeetSync

--- 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.java
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.java
@@ -1,49 +1,10 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
 class Solution {
     public int maxDepth(Node root) {
-        if(root == null) {
-            return 0;
+    	if (root == null) return 0;
+        int cur = 0;
+        for (Node node : root.children) {
+            cur = Math.max(cur, maxDepth(node));
         }
-
-        int counter = 1;
-
-        List<Node> list = new ArrayList<>(root.children);  
-
-        while(!list.isEmpty()) {
-            int size = list.size();
-
-            for(int i = 0; i < size; i++) {
-                if(list.get(size - i - 1).children != null) {
-                    list.addAll(list.get(size - i - 1).children);
-                }
-
-                list.remove(size - i - 1);
-            }
-
-            counter++;
-        }
-
-        return counter;      
+        return 1 + cur;
     }
 }
-
-
-
