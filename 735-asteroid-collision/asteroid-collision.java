d4bb593d4ab039b61e61f5f900b1799e83161b4f
David Hackro <hackro91@gmail.com>
2024-06-01 06:51:51 -0600

    Time: 6 ms (64.95%) | Memory: 45.5 MB (17.20%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        boolean flag = false;
+        
+        for(int n: asteroids) {
+            flag = false;
+
+            while(!flag && !stack.isEmpty() &&
+                    stack.peek() >= 0 &&
+                    n < 0 &&
+                    stack.peek() <= Math.abs(n))  {
+                flag = stack.pop() == Math.abs(n);
+            }
+
+            if(!stack.isEmpty() && stack.peek() >= 0 && n < 0) {
+                continue;
+            } else if(!flag) {
+                stack.push(n);
+            }
+
+        }     
+
+        return stackToArray(stack);  
+    }
+
+     public static int[] stackToArray(Stack<Integer> stack) {
+        int size = stack.size();
+        int[] array = new int[size];
+
+        // Llenar el array con los elementos de la pila
+        for (int i = 0; i < size; i++) {
+            array[i] = stack.get(i);
+        }
+
+        return array;
+    }
+}
