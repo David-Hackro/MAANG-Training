d21b2cdb349ff18b73822f7dc122e71082894135
David Hackro <hackro91@gmail.com>
2024-06-01 17:52:23 -0600

    Time: 2 ms (95.47%) | Memory: 42.2 MB (13.33%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int n: arr){
+            map.put(n, map.getOrDefault(n,0) + 1);
+        }       
+
+        return map.size() == new HashSet<>(map.values()).size();
+    }
+}
