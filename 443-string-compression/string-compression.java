9704b711517af69acddc1bbed6da1705aa424437
David Hackro <hackro91@gmail.com>
2024-06-13 09:59:10 -0600

    Time: 1 ms (99.54%) | Memory: 43.7 MB (66.31%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -4,31 +4,30 @@ class Solution {
         int r = 0;
         int i = 0;
 
-        while(r < chars.length) {
-            //["a","b","b","b","b","b","b","b","b","b","b","b","b"]
-            //
+        while (r < chars.length) {
 
-
-            while(r < chars.length && chars[l] == chars[r]) {r++;}
+            while (r < chars.length && chars[l] == chars[r]) {
+                r++;
+            }
 
             chars[i] = chars[l];
-            
-            if(r - l > 1) {
-                if(r - l > 9 ) {
+
+
+                if (r - l > 1 || r - l > 9) {
                     String number = String.valueOf((r - l));
 
-                    for(int j = 0; j < number.length(); j++) {
-                        chars[++i] = (char) (number.charAt(j));        
+                    for (int j = 0; j < number.length(); j++) {
+                        chars[++i] = (char) (number.charAt(j));
                     }
 
-                } else {
-                chars[i +1] = (char) ((r- l) + '0');
-                i++;
+                } else if(r - l != 1){
+                    chars[i + 1] = (char) ((r - l) + '0');
+                    i++;
                 }
-            }
+            
             i++;
             l = r;
-        }       
+        }
 
         return i;
     }
