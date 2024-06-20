1fb8ea24b8a2f50ff58b29830f064cfd58d99bde
David Hackro <hackro91@gmail.com>
2024-06-20 08:20:50 -0600

    Time: 1 ms (100.00%) | Memory: 44.3 MB (46.09%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -1,23 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right; 
+ *     }
+ * }
+ */
 class Solution {
-    TreeNode prev=null,first=null,second=null;
-    void inorder(TreeNode root){
-        if(root==null)
-            return ;
-        inorder(root.left);
-        if(prev!=null&&root.val<prev.val){
-            if(first==null)
-                first=prev;
-            second=root;
-        }
-        prev=root;
-        inorder(root.right);
-    }
+    private TreeNode first = null;
+    private TreeNode second = null;
+    private TreeNode prev = new TreeNode(Integer.MIN_VALUE); 
+    
     public void recoverTree(TreeNode root) {
-        if(root==null)
-            return ; 
-        inorder(root);
-        int temp=first.val;
-        first.val=second.val;
-        second.val=temp;
+        inorderTraversal(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorderTraversal(root.left);
+
+        if (first == null && prev.val > root.val) {
+            first = prev;
+        }
+
+        if (first != null && prev.val > root.val) {
+            second = root;
+        }
+
+        prev = root;
+
+        inorderTraversal(root.right);
     }
 }
