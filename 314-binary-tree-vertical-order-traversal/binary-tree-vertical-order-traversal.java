ec176611a79cdc409c66f778b40a61e53ec3eec0
David Hackro <hackro91@gmail.com>
2024-06-26 06:38:19 -0600

    Time: 5 ms (9.06%) | Memory: 42.8 MB (42.90%) - LeetSync

---
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        Queue<Integer> levels = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if(root == null) {
+            return result;
+        }
+
+        queue.add(root);
+        levels.add(0);
+        temp.add(root.val);
+        map.put(0, temp);
+    
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            temp = new ArrayList<>();
+
+            for(int i = 0; i < size; i++) {
+                TreeNode n = queue.remove();
+                int l = levels.remove();
+
+                if(n.left != null) {
+                    queue.add(n.left);
+                    levels.add(l - 1);
+                    List<Integer> a = map.getOrDefault(l - 1, new ArrayList<>());
+                    a.add(n.left.val); 
+                    map.put(l - 1, a);
+                }
+
+                if(n.right != null) {
+                    queue.add(n.right);
+                    levels.add(l + 1);
+                    List<Integer> b = map.getOrDefault(l + 1, new ArrayList<>());
+                    b.add(n.right.val); 
+                    map.put(l + 1, b);
+                }
+            }
+
+        }
+
+        Map<Integer, List<Integer>> treeMap = new TreeMap<>(map);
+
+        List<List<Integer>> list = new ArrayList<List<Integer>>(treeMap.values());
+
+        return list; 
+    }
+}
