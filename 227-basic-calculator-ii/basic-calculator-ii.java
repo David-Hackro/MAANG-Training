c23821e73d64f3fb5e635e981ef3d0c45c48c368
David Hackro <hackro91@gmail.com>
2024-06-27 14:52:38 -0600

    Time: 33 ms (15.46%) | Memory: 46.8 MB (16.21%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int calculate(String s) {
+        Deque<Integer> values = new ArrayDeque<>();
+        Deque<Character> operators = new ArrayDeque<>();
+        List<Character> simbols = new ArrayList<>();
+        simbols.add('+');
+        simbols.add('-');
+        simbols.add('*');
+        simbols.add('/');
+        int result = 0;
+
+        int i = 0;
+        int temp = 0;
+        
+        for(; i < s.length();) {
+            char c = s.charAt(i);
+
+            if(simbols.contains(c)) {
+                operators.add(c);
+                i++;
+            } 
+            else if(c == ' ') { i++;}
+            else {
+                temp = 0;
+
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    temp = temp * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                
+              if (!operators.isEmpty() && (operators.peekLast() == '*' || operators.peekLast() == '/')) {
+                    char op = operators.pollLast();
+                    int prev = values.pollLast();
+                    temp = op == '*' ? prev * temp : prev / temp;
+                }
+                
+                values.add(temp);
+
+            }
+        }
+
+        result = values.pollFirst();
+
+        while(!values.isEmpty()) {
+            if(operators.peekFirst() == '+') {
+                result += values.pollFirst();
+            } else {
+                result -= values.pollFirst();
+            }
+            operators.pollFirst();
+        }
+
+        return result;
+    }
+}
