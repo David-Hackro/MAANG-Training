de470c13fff80a44132b5e624cc4b4b5483520e8
David Hackro <hackro91@gmail.com>
2024-08-03 08:10:52 -0600

    Time: 1 ms (77.74%) | Memory: 44.6 MB (7.39%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,22 +1,15 @@
 class Solution {
-    HashMap<Integer, List<Integer>> map = new HashMap<>();
     HashSet<Integer> seen = new HashSet<>();
 
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        map.put(0, map.get(rooms.get(0)));
         seen.add(0);
         dfs(0, rooms);
     
-
         return  seen.size() == rooms.size();
     }
 
     private void dfs(int i, List<List<Integer>> rooms){
         for(int n: rooms.get(i)) {
-            if(!map.containsKey(n)) {
-                map.put(n, rooms.get(n));
-            }
-
             if(!seen.contains(n)) {
                 seen.add(n);
                 dfs(n, rooms);
