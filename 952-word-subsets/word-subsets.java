caf95702d35cc0dcf5dfc632f113cd18fb1c7807
David Hackro <hackro91@gmail.com>
2024-08-06 17:41:41 -0600

    Time: 125 ms (5.40%) | Memory: 55.3 MB (12.16%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,35 +1,43 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        int[] maxFreq = new int[26];
-        int[] tempFreq;
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        HashMap<Character, Integer> temp;
         List<String> result = new ArrayList<>();
 
-        for (String s : words2) {
-            tempFreq = new int[26];
-            for (char c : s.toCharArray()) {
-                tempFreq[c - 'a']++;
-                maxFreq[c - 'a'] = Math.max(maxFreq[c - 'a'], tempFreq[c - 'a']);
+        for(String s: words2) {
+            temp = new HashMap<>();
+
+            for(char c: s.toCharArray()) {
+                temp.put(c, temp.getOrDefault(c, 0) + 1);
+
+                if(!map.containsKey(c)) {
+                    map.put(c, 1);
+                } else if(map.get(c) < temp.get(c)) {
+                    map.put(c, temp.get(c));
+                }
             }
         }
 
-        for (String s : words1) {
-            tempFreq = new int[26];
-            for (char c : s.toCharArray()) {
-                tempFreq[c - 'a']++;
-            }
 
-            boolean isUniversal = true;
-            for (int i = 0; i < 26; i++) {
-                if (tempFreq[i] < maxFreq[i]) {
-                    isUniversal = false;
-                    break;
+        for(String s: words1) {
+            temp = new HashMap<>();
+            map2 = new HashMap<>(map);
+
+             
+            for(char c: s.toCharArray()) {
+                temp.put(c, temp.getOrDefault(c, 0) + 1);
+
+                if(map2.containsKey(c) && map2.get(c) <= temp.get(c)) {
+                    map2.remove(c);
                 }
             }
 
-            if (isUniversal) {
+            if(map2.isEmpty()) {
                 result.add(s);
             }
         }
+
 
         return result;
     }
