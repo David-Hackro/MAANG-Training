ac5380e2c26f6b6a4301db14db7dc8129972f0f1
David Hackro <hackro91@gmail.com>
2024-09-27 07:36:27 -0600

    Time: 3 ms (50.24%) | Memory: 44.6 MB (46.66%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,8 +1,13 @@
 class Solution {
+    HashMap<Integer, ArrayList<Integer>> map;
+    HashSet<Integer> seen;
+
     public boolean validTree(int n, int[][] edges) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        HashSet<Integer> seen = new HashSet<>();
-        Queue<Integer> queue = new LinkedList<>();
+        map = new HashMap<>();
+        seen = new HashSet<>();
+
+        if (edges.length != n - 1) return false;
+
 
         for (int[] array : edges) {
 
@@ -18,26 +23,20 @@ class Solution {
             map.get(array[1]).add(array[0]);
         }
 
-        queue.add(0);
-
-        while (!queue.isEmpty()) {
-            int node = queue.remove();
-
-            if (seen.contains(node)) {
-                return false;
-            }
-
-            seen.add(node);// 0,1,2,3
-
-            for (int x : map.getOrDefault(node, new ArrayList<>())) {
-
-                if (!seen.contains(x)) {
-                    queue.add(x);;
-                }
-            }
-
-        }
+        dfs(0);
 
         return seen.size() == n;
+    }
+    // Input: n = 5, edges = [[0,1],[1,2],[2,3],[1,3],[1,4]]
+
+    private void dfs(int node) {
+
+        if (seen.contains(node))
+            return;
+        seen.add(node);
+
+        for (int n : map.getOrDefault(node, new ArrayList<>())) {
+            dfs(n);
+        }
     }
 }
