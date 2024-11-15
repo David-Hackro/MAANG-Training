7cf13a6f4b8baed055343a015330e8df4e81f65b
David Hackro <hackro91@gmail.com>
2024-11-14 18:54:49 -0600

    Time: 14 ms (42.30%) | Memory: 46.3 MB (25.55%) - LeetSync

--- 761-employee-free-time/employee-free-time.java
+++ 761-employee-free-time/employee-free-time.java
@@ -1,19 +1,3 @@
-/*
-// Definition for an Interval.
-class Interval {
-    public int start;
-    public int end;
-
-    public Interval() {}
-
-    public Interval(int _start, int _end) {
-        start = _start;
-        end = _end;
-    }
-};
-
-*/
-
 class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
         List<Interval> list = new ArrayList<>();
@@ -26,18 +10,16 @@ class Solution {
         }
 
         Collections.sort(sortedList, (a,b) -> Integer.compare(a.start, b.start));
-        //[5,21],[45,56],[57,74],[89,96]
 
         Interval first = sortedList.remove(0); 
-        int start = first.start;//5
-        int end = first.end;//21
+        int start = first.start;
+        int end = first.end;
         
     
         while (!sortedList.isEmpty()) {   
             Interval current = sortedList.remove(0);
-            //[4,10]
-            if(current.start <=  end) { // is inside the range
-                end = Math.max(end, current.end);//10
+            if(current.start <=  end) { 
+                end = Math.max(end, current.end);
             } else {
                 list.add(new Interval(end, current.start));
                 start = current.start;
