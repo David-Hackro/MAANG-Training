61e129014e34ad92b69011dd5e204c7518c03310
David Hackro <hackro91@gmail.com>
2024-11-14 18:40:04 -0600

    Time: 20 ms (14.50%) | Memory: 46.4 MB (9.26%) - LeetSync

--- 761-employee-free-time/employee-free-time.java
+++ 761-employee-free-time/employee-free-time.java
@@ -12,59 +12,59 @@ class Interval {
     }
 };
 
-public class CheckFieldsOneByOne implements Comparator<Person> {
-    @Override
-    public int compare(Person o1, Person o2) {
-        int nameCompare = o1.getName().compareTo(o2.getName());
-        if(nameCompare != 0) {
-            return nameCompare;
-        }
-        return Integer.compare(o1.getAge(), o2.getAge());
-    }
-}
-
 */
 
 class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
         List<Interval> list = new ArrayList<>();
-        // Integer.compare(
-        PriorityQueue<Interval> heap = new PriorityQueue<>((a, b) -> {
-            int r = Integer.compare(a.start, b.start);
+        List<Interval> heap = new ArrayList<>();
 
-            if (r != 0) { // when is != 0 are diff
-                return r;
-            }
-
-            return Integer.compare(a.end, b.end);
-        }
-
-        );// working
+        /*
+         * PriorityQueue<Interval> heap = new PriorityQueue<>((a, b) -> {
+         * int r = Integer.compare(a.start, b.start);
+         * 
+         * if (r != 0) { // when is != 0 are diff
+         * return r;
+         * }
+         * 
+         * return Integer.compare(a.end, b.end);
+         * }
+         * 
+         * );
+         */
 
         for (List<Interval> l : schedule) {
             for (Interval interval : l) {
                 heap.add(interval);
             }
         }
+        Collections.sort(heap, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) {
+                int r = Integer.compare(a.start, b.start);
 
-        // schedule = [[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]]
-        // [,[9,12]
-        // [6,7]
+                if (r != 0) { // when is != 0 are diff
+                    return r;
+                }
+
+                return Integer.compare(a.end, b.end);
+            }
+        });
+
         Interval prev; // remove the firt (low) item
         Interval current;
         while (heap.size() > 1) {
-            prev = heap.remove();// [1,3]
-            current = heap.remove();// [2,4]
+            prev = heap.remove(0);// [1,3]
+            current = heap.remove(0);// [2,4]
 
             // if end is greather that start to the next
             if (prev.end > current.start) { // the current element is inside the prev element
-                heap.add(new Interval(prev.start, Math.max(prev.end, current.end)));
+                heap.add(0, new Interval(prev.start, Math.max(prev.end, current.end)));
             } else { // we found a common space
-                if(prev.end != current.start) {
-                    list.add(new Interval(prev.end, current.start));    
+                if (prev.end != current.start) {
+                    list.add(new Interval(prev.end, current.start));
                 }
-                
-                heap.add(current);
+
+                heap.add(0, current);
             }
         }
 
