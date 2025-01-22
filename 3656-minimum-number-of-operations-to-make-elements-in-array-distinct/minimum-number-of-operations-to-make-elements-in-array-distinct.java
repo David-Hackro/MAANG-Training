28f0444a733c858008c5a8004e740c1bb2ae9ec7
David Hackro <hackro91@gmail.com>
2025-01-22 08:13:37 -0600

    Time: 2 ms (82.76%) | Memory: 44.2 MB (91.10%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,57 +1,28 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        // Input: nums = [1,2,3,4,2,3,3,5,7]
+        HashSet<Integer> numbers = new HashSet<>();
+        //[1,2,3,4,2,3,3,5,7]
 
-        HashMap<Integer,  Integer> map = new HashMap<>();
-        int result = 0;
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        //7,5,3
+        for(int i = nums.length - 1; i >=0; i--) {
+            
+            if(numbers.contains(nums[i])) {
+                int numberElements = i + 1;
+                if(numberElements % 3 == 0) {
+                    return numberElements / 3;
+                } else {
+                    return (numberElements / 3) + 1;
+                }
+                
+                    //index == 5
+                    //5 + 1 / 3 = 
+            } 
+
+
+            numbers.add(nums[i]);
+
         }
 
-        HashSet<Integer> set = new HashSet<>(map.values());
-
-        if (set.size() == 1 && set.iterator().next() == 1) {
-            return result;
-        }
-
-        for (int i = 0; i < nums.length - 3;) {
-            result++;
-            int a = nums[i];
-            int b = nums[i + 1];
-            int c = nums[i + 2];
-
-            map.put(a, map.get(a) - 1);
-            map.put(b, map.get(b) - 1);
-            map.put(c, map.get(c) - 1);
-
-            if (map.containsKey(a) && map.get(a) == 0) {
-                map.remove(a);
-            }
-
-            if (map.containsKey(b) && map.get(b) == 0) {
-                map.remove(b);
-            }
-
-            if (map.containsKey(c) && map.get(c) == 0) {
-                map.remove(c);
-            }
-
-            set = new HashSet<>(map.values());
-
-            if (set.size() == 1 && set.iterator().next() == 1) {
-                return result;
-            }
-        
-            i+=3;
-        }
-
-        for( int n : set) {
-            if(n > 1) {
-                result++;
-            }
-        }
-        
-
-        return result;
+        return 0;
     }
 }
