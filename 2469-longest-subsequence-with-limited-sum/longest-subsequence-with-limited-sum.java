ef02f8bedc6c0410ae5bad50ed83f6749cf7d357
David Hackro <hackro91@gmail.com>
2025-03-06 07:29:06 -0600

    Time: 5 ms (98.92%) | Memory: 45.2 MB (30.72%) - LeetSync

---
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        int[] ans = new int[queries.length];
+        int[] prefix = new int[nums.length];
+    
+
+        Arrays.sort(nums);
+
+        prefix[0] = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            prefix[i] = nums[i] + prefix[i -1]; 
+        }
+
+        for(int i = 0; i < queries.length; i++) {
+            ans[i] = getMaxSubSequence(prefix, queries[i]);
+        }
+
+        return ans;
+    }
+
+
+    private int getMaxSubSequence(int[] prefix, int query) {
+        int l = 0;
+        int r = prefix.length -1;
+        int m = (l + r) / 2;
+        int last = 0;
+
+        while(l <= r) {
+            m = (l + r) / 2;
+            //10
+            //1,3,7,12
+            //     l,r
+            if(prefix[m] == query) {
+                return m + 1;
+            }
+
+            if(prefix[m] > query) {
+                r = m - 1;
+            }  else {
+                last = m + 1;
+                l = m + 1;
+            }
+        }
+
+        return last;
+    }
+}
