a8069a5888256858d382a62a393a5927426a908d
David Hackro <hackro91@gmail.com>
2025-03-09 09:02:52 -0600

    Time: 27 ms (24.20%) | Memory: 44.9 MB (87.61%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,36 +1,34 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        
-        int l = 1;//min speed
-        int mid = 0;
-        int r = 0;//max speed
-        int counter = 0;
+      int l = 0;
+      int r = 0;
+      int counter = 0;
+      int m = 0;
+      int result = Integer.MAX_VALUE;
+      
+      for(int n: piles) {
+        r = Math.max(r, n);
+      }  
 
-        for(int p: piles) {
-            r = Math.max(r, p);
-            l = Math.min(l, p);
+      while(l <= r) {
+        counter = 0;
+         m = (l + r) / 2;
+
+        for(int i = 0; i < piles.length; i++) {
+            counter += Math.ceil((double) piles[i] / m);
         }
+        
+        if(counter <= h) {
+            result =  m;
+        }
+        
+        if(counter <= h) {
+            r = m -1;
+        } else {
+            l = m + 1;
+        }
+      }
 
-        if(piles.length == h) {
-            return r;
-        }      
-
-        while(l <= r)  {
-            mid = l + ((r - l) / 2);
-            counter = 0;
-
-            for(int p : piles) {
-                counter += (int) Math.ceil((double) p / mid);
-                if(counter > h) break;
-            }
-
-            if(counter > h) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        } 
-
-        return l;
+      return result;
     }
 }
