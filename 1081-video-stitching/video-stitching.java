a3d0aa9fd2f10149bb3d74a8db8ccffbea02f899
David Hackro <hackro91@gmail.com>
2025-03-25 17:06:06 -0600

    Time: 1 ms (83.22%) | Memory: 41.5 MB (36.51%) - LeetSync

--- 1081-video-stitching/video-stitching.java
+++ 1081-video-stitching/video-stitching.java
@@ -1,21 +1,15 @@
 class Solution {
     public int videoStitching(int[][] clips, int time) {
         int pointer = 0;
-        int i = 0; // indice for the clips array
+        int i = 0;
         int counter = 0;
 
         Arrays.sort(clips, (a, b) -> {
             int lengthA = a[1] - a[0] + 1;
             int lengthB = b[1] - b[0] + 1;
-            // [0,2],[0,6]
-
             if (a[0] != b[0]) {
                 return Integer.compare(a[0], b[0]);
             }
-
-            // Negative value (less than 0): If x is less than y.
-            // Zero (0): If x is equal to y.
-            // Positive value (greater than 0): If x is greater than y.
 
             if (lengthA == lengthB) {
                 return 0;
@@ -26,36 +20,27 @@ class Solution {
 
         while (pointer < time && i < clips.length) {
             boolean flag = false;
-            // search the first element that the finish value is greather
-            // that my current pointer
-
+           
             int max = i;// this is an indice
 
             while (i < clips.length && clips[i][0] <= pointer) {
 
-                if (clips[i][1] > pointer) {
-
-                    if (clips[i][1] - pointer + 1 >= clips[max][1] - pointer + 1) {
-                        max = i;
-                    }
+                if (clips[i][1] > pointer && clips[i][1] - pointer + 1 >= clips[max][1] - pointer + 1) {
+                    max = i;
                     flag = true;
                 }
+
                 i++;
             }
 
             if (flag) {
-                System.out.println(clips[max][0] + "," + clips[max][1]);
                 counter++;
                 pointer = clips[max][1];
             } else {
-                 i++;
+                i++;
             }
         }
 
-        if (pointer >= time) {
-            return counter;
-        }
-
-        return -1;
+        return pointer >= time?counter:-1;
     }
 }
