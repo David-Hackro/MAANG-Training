c9a259d4fb45d6c0fcf3054492a077b4bc6098e2
David Hackro <hackro91@gmail.com>
2025-03-28 11:57:55 -0600

    Time: 17 ms (80.68%) | Memory: 56.6 MB (35.46%) - LeetSync

--- 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
@@ -2,21 +2,20 @@ class Solution {
     public int eliminateMaximum(int[] dist, int[] speed) {
         int turns = 0;
         int monsters = 0;
-
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> {
-            return Integer.compare(a, b);
-        });
+        int index  = 0;
 
         for (int i = 0; i < dist.length; i++) {
             int residual = dist[i] % speed[i] == 0 ? 0 : 1;
             int turn = (int) (dist[i] / speed[i]) + residual;
-            maxHeap.add(turn);
+            dist[i] = turn;
         }
 
-        while (!maxHeap.isEmpty() && maxHeap.peek() > turns) {
-            maxHeap.remove();
+        Arrays.sort(dist);
+
+        while (index < dist.length && dist[index] > turns) {
             monsters++;
             turns++;
+            index++;
         }
 
         if (monsters != dist.length) {
